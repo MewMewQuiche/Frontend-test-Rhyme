cd333065e4897cc9623ef9ae27782b2e7a666a90
Yue <yue.liljeblad@gmail.com>
2019-10-04 18:50:28 +0200

    APIs are working

--- src/serviceAPI.js
+++ src/serviceAPI.js
@@ -1,68 +1,47 @@
-export class ArticlesHandler {
-	
-	state = {
-			selectedArticle,
-			articles: []
-	}
-	
-	constructor(url){
-		//this.url = url;
-		this.url = 'http://ec2-34-245-55-16.eu-west-1.compute.amazonaws.com';
-	}
+export async function getArticles(url) {
+	return fetch(url + '/articles')
+	   	.then(res => res.json())
+   		.then((data) => {
+   	  		console.log(data)
+		})
+   		.catch((err) => console.log(err));
+}
 
-	async getArticles() {
-		return fetch(this.url + '/articles')
-    	.then(res => res.json())
-    	.then((data) => {
-      	this.setState({ articles: data })
-    	  	console.log(this.state.articles)
-   		})
-    	.catch(console.log);
-    	return this.state.articles;
- 	}
+export async function getArticle(url, id){
+	return fetch(url + '/articles/' + id)
+	   	.then(res => res.json())
+   		.then((data) => {
+   	  		console.log(data)
+		})
+   		.catch((err) => console.log(err));
+}
 
-	async getArticle(id){
-		fetch(this.url + '/articles/' + id)
-    	.then(res => res.json())
-    	.then((data) => {
-      	this.setState({ selectedArticle: data })
-    	  	console.log(this.state.selectedArticle)
-   		})
-    	.catch(console.log);
-    	return this.state.selectedArticle;
-    }
+export async function createArticle(url, title, description, author, body){ 
+	fetch(url + '/articles', {
+    	method: 'POST',
+		headers : {'Content-Type': 'application/json'},
+        body: JSON.stringify({'title':title, 'description':description, 'author':author, 'body':body})
+    }).then((res) => console.log(res))
+    .then((data) => console.log('success'))
+    .catch((err) => console.log(err))
+}
 
-	async createArticle(title, description, author, body){
-		event.preventDefault();
-        fetch(this.url + '/articles', {
-        	method: 'POST',
-        	headers : new Headers(),
-        	body:JSON.stringify({title:title, description:description, author:author, body:body})
-        }).then((res) => res.json())
-        .then((data) =>  console.log(data))
-        .catch((err)=>console.log(err))
-	}
+export async function updateArticle(url, id, title, description, author, body){
+	fetch(url + '/articles/' + id, {
+    	method: 'PUT',
+       	headers : {'Content-Type': 'application/json'},
+       	body:JSON.stringify({'_id':id, 'title':title, 'description':description, 'author':author, 'body':body})
+    }).then((res) => console.log(res))
+    .then((data) => console.log('success'))
+    .catch((err) => console.log(err))
+}
 
-	async updateArticle(id, title, description, author, body){
-		event.preventDefault();
-        fetch(this.url + '/articles/' + id, {
-        	method: 'PUT',
-        	headers : new Headers(),
-        	body:JSON.stringify({_id:id, title:title, description:description, author:author, body:body})
-        }).then((res) => res.json())
-        .then((data) =>  console.log(data))
-        .catch((err)=>console.log(err))
-	}
-
-	async deleteArticle(id){
-		event.preventDefault();
-        fetch(this.url + '/articles/' + id, {
-        	method: 'DELETE',
-        	headers : new Headers(),
-        	body:JSON.stringify({id})
-        }).then((res) => res.json())
-        .then((data) =>  console.log(data))
-        .catch((err)=>console.log(err))
-	}
+export async function deleteArticle(url, id){
+	fetch(url + '/articles/' + id, {
+       	method: 'DELETE',
+       	headers : new Headers()
+    }).then((res) => console.log(res))
+    .then((data) => console.log('success'))
+    .catch((err) => console.log(err))
 }
 

c454de8d76121e4fc464b90c3e942081aff2ce28
Yue <yue.liljeblad@gmail.com>
2019-10-10 20:44:33 +0200

    createArticle, updateArticle and deleteArticle working

--- src/pages/createEditArticle.jsx
+++ src/pages/createEditArticle.jsx
@@ -8,73 +8,88 @@ export class CreateEditArticle extends React.Component{
 		article: {},
 		url: '',
 		titleText: 'Write an article',
-		buttons: ''
+		buttons: '',
+		articleBackup: {}
 	};
 		
 	constructor(props) {
 		super(props);
 
 		this.state.url = 'http://ec2-34-245-55-16.eu-west-1.compute.amazonaws.com';// should be from settings		
-		if(props.location !== undefined && props.location.article !== undefined && props.location.article != null){
-			this.state.article = props.location.article;//bug - looses track of history - solution redux?
+		if(props.location !== undefined && props.location.article !== undefined && props.location.article !== null && props.location.article._id !== undefined){
+			this.state.article = props.location.article;//bug - looses track of history and on reloads - solution redux?
 			this.state.titleText = 'Edit article';
+			this.state.articleBackup = this.state.article;//In case of canceling the edit then changes are discared.
 		}
-
-		this.handleSubmit = this.submit.bind(this);
+		else if(false){//fetch from localstorage||redux presist store
+			//this.state.article = getArticle(this.state.url, articleId);
+			//this.state.titleText = 'Edit article';
+		}
+		else{
+			this.state.article = {title: '', description: '', body: '', author: ''};
+		}
 
 		this.generateButtons();
 	}
 
-	submit(event) {//TODO
-		console.log('submit');
-		console.log(event);
-		alert(event);
-		//If Delete
-		//	if(window.confirm('Are you sure you wish to delete this item?')) this.confirmedDeleteArticle()}}
-		//		this.confirmedDeleteArticle();
-		//		force route back to articles
-		//if Save
-		/*
+	generateButtons(){
+		if(this.state.article._id !== undefined && this.state.article._id !== ''){
+			this.state.buttons = <div className="row">
+					<button><Link to='/'>Back to articles</Link></button>
+					<button onClick={this.deleteHandler}>Delete article</button>
+					<button><Link to={{ pathname: "/ReadArticle", article: this.state.articleBackup }}>Cancel</Link></button>
+					<button onClick={this.submitHandler}>Save</button>
+				</div>;
+		}
+		else{
+			this.state.buttons = <div className="row">
+					<button><Link to='/'>Cancel</Link></button>
+					<button onClick={this.submitHandler}>Save</button>
+				</div>;
+		}
+	}
+
+	deleteHandler = event => {
+		event.preventDefault();
+		if (window.confirm('Are you sure you wish to delete this article?')) {
+			if(this.state.article._id !== undefined && this.state.article._id !== ''){
+				deleteArticle(this.state.url, this.state.article._id);
+			}
+			//TODO force route back to list
+		}
+	}
+
+	changeHandler = event => {
+		const name = event.target.name;
+		const value = event.target.value;
+		
 		this.setState({
-  			[author]: author
-  		});
+			article: {
+				...this.state.article,
+				[name]: value
+			}
+		});
+	}
+
+	submitHandler = event => {//Why do changeHandler and submitHandler require = event => in order for this to be defined?
+		event.preventDefault();
+		let errorExists = false;
+		
 		if(this.state.article._id !== undefined && this.state.article._id !== ''){
 			updateArticle(this.state.url, this.state.article._id, this.state.article.title, this.state.article.description, this.state.article.author, this.state.article.body);
 		}
 		else if(this.state.article.title !== ''){
 			createArticle(this.state.url, this.state.article.title, this.state.article.description, this.state.article.author, this.state.article.body);
 		}
-		*/
-		event.preventDefault();
-
-		//TODO force route. if (article._id and (cancel || save)) then return to article, else return to articles list
-	}
-
-	confirmedDeleteArticle(){//TODO
-			console.log('deleted1');
-		if(this.state.article._id !== undefined && this.state.article._id !== ''){
-			console.log('deleted2');
-			//TODO make nicer confirm delte alart
-			deleteArticle(this.state.url, this.state.article._id);
-			console.log('deleted3');
-			//TODO force route back to articles
+		else{
+			errorExists = true;
 		}
-	}
-
-	generateButtons(){
-		if(this.state.article._id !== undefined && this.state.article._id !== ''){
-			this.state.buttons = <div className="row">
-					<input type="submit" value="Back to articles" />
-					<input type="submit" value="Delete article" />
-					<input type="submit" value="Cancel" />
-					<input type="submit" value="Save" />
-				</div>;
+		
+		if(!errorExists){
+			console.log('routes to readArticle or list now?');
 		}
 		else{
-			this.state.buttons = <div className="row">
-					<input type="submit" value="Cancel" />
-					<input type="submit" value="Save" />
-				</div>;
+			console.log('some error message to user here delivered in some nice way');
 		}
 	}
 
@@ -82,26 +97,26 @@ export class CreateEditArticle extends React.Component{
 		return (
 			<form className="container">
 				<div>
-					<h2>{this.state.titleText}</h2>
+					<h2>{this.state.titleText}</h2>title description body author
 				</div>
 				<div className="row">
-					<div className="card col-12" onSubmit={this.submit}>
+					<div className="card col-12">
 						<div className="card-body">
 							<label className="card-title block">
 								Title<br />
-								<input type="text" value={this.state.article.title} name="title" />
+								<input type="text" value={this.state.article.title} onChange={this.changeHandler} name="title" />
 							</label>
 							<label className="card-text block">
 								Description<br />
-								<input type="text" value={this.state.article.description} name="description" />
+								<input type="text" value={this.state.article.description} onChange={this.changeHandler} name="description" />
 							</label>
 							<label className="card-text block">
 								Content<br />
-								<textarea value={this.state.article.body} name="body" />
+								<textarea value={this.state.article.body} onChange={this.changeHandler} name="body" />
 							</label>
 							<label className="card-text block">
 								Author<br />
-								<input type="text" value={this.state.article.author} name="author" />
+								<input type="text" value={this.state.article.author} onChange={this.changeHandler} name="author" />
 							</label>
 						</div>
 					</div>
--- src/pages/readArticle.jsx
+++ src/pages/readArticle.jsx
@@ -1,27 +1,45 @@
 import React from "react";
+//import {getArticle} from '../services/serviceAPI';
 import {Link} from "react-router-dom";
 
-export const ReadArticle = (props) => {
-	const article = props.location.article || {};//bug - looses track of history - solution redux?
+export class ReadArticle extends React.Component{
+	state = {
+		article: {},
+		url: ''
+	};
+		
+	constructor(props) {
+		super(props);
 
-	return (
-		<div className="container">
-			<div className="row">
-				<div className="card col-12">
-					<div className="card-body">
-						<h5 className="card-title">{article.title}</h5>
-						<p className="card-text">{article.description}</p>
-						<p className="card-text">{article.body}</p>
-						<small className="card-text text-muted">Author: <i>{article.author}</i></small>
+		this.state.url = 'http://ec2-34-245-55-16.eu-west-1.compute.amazonaws.com';// should be from settings		
+		if(props.location !== undefined && props.location.article !== undefined && props.location.article != null){
+			this.state.article = props.location.article;//bug - looses track of history and on reloads - solution redux?
+		}
+		else if(false){//fetch from ?articleId=dlkflkdfjglkgggkmdkfdkfkk
+			//this.state.article = getArticle(this.state.url, articleId);
+		}
+	}
+
+	render(){
+		return (
+			<div className="container">
+				<div className="row">
+					<div className="card col-12">
+						<div className="card-body">
+							<h5 className="card-title">{this.state.article.title}</h5>
+							<p className="card-text">{this.state.article.description}</p>
+							<p className="card-text">{this.state.article.body}</p>
+							<small className="card-text text-muted">Author: <i>{this.state.article.author}</i></small>
+						</div>
 					</div>
 				</div>
+				<div className="row">
+					<button><Link to='/'>Back to articles</Link></button>
+					<button>
+						<Link to={{ pathname: "/EditArticle", article: this.state.article }}>Edit article</Link>
+					</button>
+				</div>
 			</div>
-			<div className="row">
-				<button><Link to='/'>Back to articles</Link></button>
-				<button>
-					<Link to={{ pathname: "/EditArticle", article: article }}>Edit article</Link>
-				</button>
-			</div>
-		</div>
-	);
-};
+		);
+	}
+}
